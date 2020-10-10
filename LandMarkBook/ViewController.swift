25de45e92353949174e509767c34137e38013dff
Ali Cantürk <canturk.ali@gmail.com>
2020-10-10 19:53:42 +0300

    Initial Commit

---
+++ LandMarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMarkBook
+//
+//  Created by Ali Cantürk on 10.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
